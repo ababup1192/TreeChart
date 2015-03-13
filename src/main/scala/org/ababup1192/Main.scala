b826644f3922e01f2976a0092b790380a6e9c731
ababup1192 <ababup1192@gmail.com>
2015-03-13 21:56:21 +0900

    Refactor TreePane

--- src/main/scala/org/ababup1192/Main.scala
+++ src/main/scala/org/ababup1192/Main.scala
@@ -12,6 +12,7 @@ object Main extends JFXApp {
     val treePane = new TreePane()
     treePane.yAxisSpacingProperty() = 10d
 
+    /*
     treePane.addChild(Label("ROOT"), NodePosition.ROOT)
     treePane.addChild(Label("0"), NodePosition.ROOT.child(0))
 
@@ -22,22 +23,22 @@ object Main extends JFXApp {
     treePane.addChild(Label("02"), NodePosition.ROOT.child(0).child(2))
     treePane.addChild(Label("010"), NodePosition.ROOT.child(0).child(1).child(0))
     treePane.addChild(Label("011"), NodePosition.ROOT.child(0).child(1).child(1))
+    */
 
-    /*
-    treePane.addChild(PackageBox("ROOT"), NodePosition.ROOT)
+    treePane.addChild(PackageBox("DefaultPackage"), NodePosition.ROOT)
 
     treePane.addChild(PackageBox("pack_a"), NodePosition.ROOT.child(0))
     treePane.addChild(PackageBox("pack_b"), NodePosition.ROOT.child(1))
     treePane.addChild(PackageBox("pack_b_a"), NodePosition.ROOT.child(1).child(0))
     treePane.addChild(PackageBox("pack_a_a"), NodePosition.ROOT.child(0).child(1))
-    */
+
 
     val root = new ScrollPane {
       content = treePane
     }
 
     scene = new Scene(root, 900, 700)
-    scene.value.getStylesheets.add(getClass.getResource("/style01.css").toString)
+    // scene.value.getStylesheets.add(getClass.getResource("/style01.css").toString)
 
   }
 
--- src/main/scala/org/ababup1192/TreePane.scala
+++ src/main/scala/org/ababup1192/TreePane.scala
@@ -11,8 +11,8 @@ import scalafx.scene.layout.Pane
 import scalafx.scene.shape.Line
 
 class TreePane extends Pane {
-  val yAxisSpacing = 200d
-  val xAxisSpacing = 200d
+  val yAxisSpacing = 100d
+  val xAxisSpacing = 100d
   val lineSpacing = 2d
   var isShowLines = true
 
@@ -21,14 +21,11 @@ class TreePane extends Pane {
   val lineSpacingProperty: DoubleProperty = new DoubleProperty()
   val showLinesProperty: BooleanProperty = new BooleanProperty()
 
-  // style = "tree-pane"
-
   minHeight.bind(prefHeight)
   maxHeight.bind(prefHeight)
 
   minWidth.bind(prefWidth)
   maxWidth.bind(prefWidth)
-
 
   private val spacingChangeListener = new ChangeListener[Number] {
     override def changed(observable: ObservableValue[_ <: Number], oldValue: Number, newValue: Number): Unit = {
@@ -51,7 +48,7 @@ class TreePane extends Pane {
   private val nodeByPosition: MMap[NodePosition, Node] = new MHashMap[NodePosition, Node]()
   private val positionByNode: MMap[Node, NodePosition] = new MHashMap[Node, NodePosition]()
   private val nodeByParentPosition: MMap[NodePosition, MSet[Node]] = new MHashMap[NodePosition, MSet[Node]]()
-  private val nodeByLevel: MMap[Integer, MSet[Node]] = new MHashMap[Integer, MSet[Node]]
+  private val nodeByLevel: MMap[Int, MSet[Node]] = new MHashMap[Int, MSet[Node]]
 
   var lines = Seq[Line]()
 
@@ -68,74 +65,33 @@ class TreePane extends Pane {
       val positionsByLevel = new MHashMap[Int, MSet[NodePosition]]
       val positionsByParentPosition = new MHashMap[NodePosition, MSet[NodePosition]]
 
-      val maxLevel = nodeByLevel.keys.max.toInt
+      val maxLevel = nodeByLevel.keys.max
 
       (0 to maxLevel).reverse.foreach { level =>
         levelHeight.put(level, 0d)
         positionsByLevel.put(level, MSet[NodePosition]())
       }
 
-
       (0 to maxLevel).reverse.foreach { level =>
-        nodeByLevel.get(level).foreach { nodes =>
-          nodes.foreach { node =>
-            positionByNode.get(node).foreach { position =>
-              val nodeBounds = node.layoutBounds
-              widthByPosition.put(position, nodeBounds.value.getWidth + this.xAxisSpacing)
-              levelHeight.put(level, Math.max(levelHeight.get(level).get, nodeBounds.value.getHeight + this.yAxisSpacing))
 
-              positionsByLevel.get(level).foreach(_.add(position))
-              if (level > 0) {
-                positionsByLevel.get(level - 1).foreach(_.add(position.parent))
-              }
-            }
+        setNodeLayoutByLevel(level, widthByPosition, levelHeight, positionsByLevel)
+
+        positionsByLevel.get(level).foreach { positions =>
+          positions.foreach { position =>
+            setWidthByLevel(position, positionsByLevel, positionsByParentPosition, widthByPosition)
           }
         }
-
-        positionsByLevel.get(level).foreach(positions =>
-          positions.foreach { position =>
-            if (position.level > 0) {
-              val parentPosition = position.parent
-              positionsByLevel.get(position.level - 1).foreach(_.add(parentPosition))
-              positionsByParentPosition.get(parentPosition).map {
-                _.add(position)
-              }.getOrElse {
-                positionsByParentPosition.put(parentPosition, MSet(position))
-              }
-            }
-
-            var widthOfChildren = 0d
-            positionsByParentPosition.get(position).foreach {
-              parentPositions =>
-                parentPositions.foreach { childPosition =>
-                  widthByPosition.get(childPosition).foreach { width =>
-                    widthOfChildren += width
-                  }
-                }
-            }
-            widthByPosition.get(position).map { width =>
-              widthByPosition.put(position, Math.max(width, widthOfChildren))
-            }.getOrElse {
-              widthByPosition.put(position, widthOfChildren)
-            }
-          }
-        )
       }
+
       val boxesByPosition = new MHashMap[NodePosition, Rectangle2D]()
+
+      // ROOT Region
       boxesByPosition.put(NodePosition.ROOT, new Rectangle2D(0d, 0d, widthByPosition.get(NodePosition.ROOT).get, levelHeight.get(0).get))
 
       (0 to maxLevel).foreach { level =>
         positionsByLevel.get(level).foreach { positions =>
           positions.foreach { position =>
-            val positionBox = boxesByPosition(position)
-            val childPositions = positionsByParentPosition.getOrElse(position, List()).toList.sorted
-            var childX = positionBox.getMinX
-            childPositions.foreach { childPosition =>
-              widthByPosition.get(childPosition).foreach { childWidth =>
-                boxesByPosition.put(childPosition, new Rectangle2D(childX, positionBox.getMaxY, childWidth, levelHeight(childPosition.level)))
-                childX += childWidth
-              }
-            }
+            setPositionBoxes(position, boxesByPosition, positionsByParentPosition, widthByPosition, levelHeight)
           }
         }
       }
@@ -145,22 +101,7 @@ class TreePane extends Pane {
       (0 to maxLevel).reverse.foreach { level =>
         positionsByLevel.get(level).foreach {
           _.foreach { position =>
-            val positionBox = boxesByPosition(position)
-            val xCenterHint = xCenterHintByPosition.getOrElse(position, (positionBox.getMinX + positionBox.getMaxX) / 2d)
-            val yCenterHint = (positionBox.getMinY + positionBox.getMaxY) / 2d
-
-            xCenterHintByPosition.put(position, xCenterHint)
-            yCenterHintByPosition.put(position, yCenterHint)
-            nodeByPosition.get(position).foreach { node =>
-              val nodeBounds = node.layoutBounds.value
-              node.relocate(xCenterHint - nodeBounds.getWidth / 2d, yCenterHint - nodeBounds.getHeight / 2d)
-            }
-            val parentPosition = position.parent
-            xCenterHintByPosition.get(parentPosition).map { parentXCenterHint =>
-              xCenterHintByPosition.put(parentPosition, (parentXCenterHint + xCenterHint) / 2d)
-            }.getOrElse {
-              xCenterHintByPosition.put(parentPosition, xCenterHint)
-            }
+            relocateNode(position, boxesByPosition, xCenterHintByPosition, yCenterHintByPosition)
           }
         }
       }
@@ -171,31 +112,8 @@ class TreePane extends Pane {
         boxesByPosition.keys.foreach { position =>
           positionsByParentPosition.get(position).foreach { childPositions =>
             childPositions.foreach { childPosition =>
-              // println(nodeByPosition.get(position).get.layoutBounds)
-              val fromBounds = nodeByPosition.get(position).map(node => Some(node.boundsInLocal.value)).getOrElse(None)
-              val toBounds = nodeByPosition.get(childPosition).map(node => Some(node.boundsInLocal.value)).getOrElse(None)
-              val lineFrom = new Point2D(
-                xCenterHintByPosition(position) + fromBounds.map(_.getWidth / 2d).getOrElse(0d),
-                yCenterHintByPosition(position) + fromBounds.map(_.getHeight / 2d).getOrElse(0d) + this.lineSpacing
-              )
-
-              val lineTo = new Point2D(
-                xCenterHintByPosition(childPosition) + toBounds.map(_.getWidth / 2d).getOrElse(0d),
-                yCenterHintByPosition(childPosition) + toBounds.map(_.getHeight / 2d).getOrElse(0d) + this.lineSpacing
-              )
-              try {
-                val line = lines(currentLine)
-
-                line.setStartX(lineFrom.getX)
-                line.setStartY(lineFrom.getY)
-
-                line.setEndX(lineTo.getX)
-                line.setEndY(lineTo.getY)
-
-                currentLine += 1
-              } catch {
-                case e: Exception =>
-              }
+              drawLines(currentLine, position, childPosition, xCenterHintByPosition, yCenterHintByPosition)
+              currentLine += 1
             }
           }
         }
@@ -206,23 +124,145 @@ class TreePane extends Pane {
       prefWidth = widthByPosition.get(NodePosition.ROOT).get
       prefHeight = totalHeight
     }
+  }
 
+  def setNodeLayoutByLevel(level: Int, widthByPosition: MHashMap[NodePosition, Double],
+                           levelHeight: MHashMap[Int, Double], positionsByLevel: MHashMap[Int, MSet[NodePosition]]) = {
+    nodeByLevel.get(level).foreach { nodeSet =>
+      nodeSet.foreach { node =>
+        positionByNode.get(node).foreach { position =>
+          val nodeBounds = node.boundsInLocal.value
+
+          // Set width and height
+          widthByPosition.put(position, nodeBounds.getWidth + this.xAxisSpacing)
+          levelHeight.put(level, Math.max(levelHeight.get(level).get, nodeBounds.getHeight + this.yAxisSpacing))
+
+          // Adding position
+          positionsByLevel.get(level).foreach {
+            _.add(position)
+          }
+          // Adding Parent Position
+          if (level > 0) {
+            val parentLevel = level - 1
+            positionsByLevel.get(parentLevel).foreach {
+              _.add(position.parent)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  def setWidthByLevel(position: NodePosition, positionsByLevel: MHashMap[Int, MSet[NodePosition]],
+                      positionsByParentPosition: MHashMap[NodePosition, MSet[NodePosition]],
+                      widthByPosition: MHashMap[NodePosition, Double]) = {
+    if (position.level > 0) {
+      // Add ParentPosition
+      val parentPosition = position.parent
+      val parentLevel = position.level - 1
+
+      positionsByLevel.get(parentLevel).foreach {
+        _.add(parentPosition)
+      }
+
+      positionsByParentPosition.get(parentPosition).map {
+        _.add(position)
+      }.getOrElse {
+        positionsByParentPosition.put(parentPosition, MSet(position))
+      }
+    }
+
+    // Calculation width by Level
+    var widthOfChildren = 0d
+    positionsByParentPosition.get(position).foreach {
+      parentPositions =>
+        parentPositions.foreach { childPosition =>
+          widthByPosition.get(childPosition).foreach { width =>
+            widthOfChildren += width
+          }
+        }
+    }
+    // Set width of position
+    widthByPosition.get(position).map { width =>
+      widthByPosition.put(position, Math.max(width, widthOfChildren))
+    }.getOrElse {
+      widthByPosition.put(position, widthOfChildren)
+    }
+  }
+
+  def setPositionBoxes(position: NodePosition, boxesByPosition: MHashMap[NodePosition, Rectangle2D],
+                       positionsByParentPosition: MHashMap[NodePosition, MSet[NodePosition]], widthByPosition: MHashMap[NodePosition, Double],
+                       levelHeight: MHashMap[Int, Double]) = {
+    val positionBox = boxesByPosition(position)
+    val childPositions = positionsByParentPosition.getOrElse(position, List()).toList.sorted
+    var childX = positionBox.getMinX
+
+    childPositions.foreach { childPosition =>
+      widthByPosition.get(childPosition).foreach { childWidth =>
+        // Set boxes
+        boxesByPosition.put(childPosition, new Rectangle2D(childX, positionBox.getMaxY, childWidth, levelHeight(childPosition.level)))
+        childX += childWidth
+      }
+    }
+  }
+
+  def relocateNode(position: NodePosition, boxesByPosition: MHashMap[NodePosition, Rectangle2D],
+                   xCenterHintByPosition: MHashMap[NodePosition, Double], yCenterHintByPosition: MHashMap[NodePosition, Double]) = {
+    val positionBox = boxesByPosition(position)
+    val xCenterHint = xCenterHintByPosition.getOrElse(position, (positionBox.getMinX + positionBox.getMaxX) / 2d)
+    val yCenterHint = (positionBox.getMinY + positionBox.getMaxY) / 2d
+
+    xCenterHintByPosition.put(position, xCenterHint)
+    yCenterHintByPosition.put(position, yCenterHint)
+    nodeByPosition.get(position).foreach { node =>
+      val nodeBounds = node.layoutBounds.value
+      node.relocate(xCenterHint - nodeBounds.getWidth / 2d, yCenterHint - nodeBounds.getHeight / 2d)
+    }
+    val parentPosition = position.parent
+    xCenterHintByPosition.get(parentPosition).map { parentXCenterHint =>
+      xCenterHintByPosition.put(parentPosition, (parentXCenterHint + xCenterHint) / 2d)
+    }.getOrElse {
+      xCenterHintByPosition.put(parentPosition, xCenterHint)
+    }
+  }
+
+  def drawLines(currentLine: Int, position: NodePosition, childPosition: NodePosition,
+                xCenterHintByPosition: MHashMap[NodePosition, Double], yCenterHintByPosition: MHashMap[NodePosition, Double]) = {
+    val fromBounds = nodeByPosition.get(position).map(node => Some(node.boundsInLocal.value)).getOrElse(None)
+    val toBounds = nodeByPosition.get(childPosition).map(node => Some(node.boundsInLocal.value)).getOrElse(None)
+    val lineFrom = new Point2D(
+      xCenterHintByPosition(position) + fromBounds.map(_.getWidth / 2d).getOrElse(0d),
+      yCenterHintByPosition(position) + fromBounds.map(_.getHeight / 2d).getOrElse(0d) + this.lineSpacing
+    )
+
+    val lineTo = new Point2D(
+      xCenterHintByPosition(childPosition) + toBounds.map(_.getWidth / 2d).getOrElse(0d),
+      yCenterHintByPosition(childPosition) + toBounds.map(_.getHeight / 2d).getOrElse(0d) + this.lineSpacing
+    )
+    try {
+      val line = lines(currentLine)
+
+      line.setStartX(lineFrom.getX)
+      line.setStartY(lineFrom.getY)
+
+      line.setEndX(lineTo.getX)
+      line.setEndY(lineTo.getY)
+    } catch {
+      case e: IndexOutOfBoundsException =>
+        println(e)
+    }
   }
 
   def adjustLineCount(count: Int) = {
     while (count < lines.size) {
       children.remove(lines.size - 1)
-      // this.parent.value.getChildrenUnmodifiable.remove(lines.last)
       lines = lines.dropRight(1)
     }
 
     while (count > lines.size) {
-      val line = new Line {
-        // style = "tree-pane-line"
-      }
+      val line = new Line
 
       children.add(line)
-      // this.parent.value.getChildrenUnmodifiable.add(line)
       line.toBack()
       lines = lines :+ line
     }
@@ -231,7 +271,6 @@ class TreePane extends Pane {
   def addChild(node: Node, position: NodePosition) {
     if (!this.getChildrenUnmodifiable.contains(node)) {
       children.add(node)
-      // parent.value.getChildrenUnmodifiable.add(node)
       node.toFront()
     }
     setPosition(node, position)
